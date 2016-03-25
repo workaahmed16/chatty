941f7b54fa25089293e75f174758aa174678d87e
Ahmed Ahmed <critenks29@gmail.com>
2016-03-25 19:38:27 +0000

    Initialize Repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Chatty::Application.config.secret_key_base = '39ade88b00de20cb5fca5fb96e6fecdcdf4a4be5770fd0468635fbed3a16043fc8733a01ed24625f5f4ed52e5c46931289608f8a9c9404ae6113b3fcb20002de'
